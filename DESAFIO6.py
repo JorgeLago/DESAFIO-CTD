7d9725b253957426946c2bc1cc761ae369845272
Jorge Lago <jorgelago92@gmail.com>
2022-09-24 12:30:57 -0300

    Iniciando desafio dia 06

---
+++ DESAFIO6.py
@@ -0,0 +1,1 @@
+# Faça um programa que recebe o valor dos catetos de um triangulo e imprima na tela o valor da hipotenusa. Lembre-se que: h² = a²+b², sendo h - hipotenusa a e b- catetos do triângulo.
