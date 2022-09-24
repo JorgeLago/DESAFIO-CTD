0c85736783aec380d1ecafa7cd8746b0cfa3da03
Jorge Lago <jorgelago92@gmail.com>
2022-09-24 12:20:50 -0300

    Desafio dia 5 - Concluído

--- DESAFIO5.py
+++ DESAFIO5.py
@@ -1,4 +1,7 @@
 #Faça um programa que receba um número inteiro positivo N e calcule o somatório de 1 até N. Imprima na tela o resultado da soma.
 
-num=abs(input("digite um numero:  "))
-
+num=int(input("digite um numero:  "))
+soma=0
+for i in range(1,num+1):
+    soma+=i
+print("O resultado da soma de 1 até "+str(num) + " é " + str(soma))
