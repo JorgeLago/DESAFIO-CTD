a2ad812b3935670ec523fcccf557df2fff9296d3
Jorge Lago <jorgelago92@gmail.com>
2022-09-26 00:10:04 -0300

    DESAFIO DIA 6 - CONCLUÍDO

--- DESAFIO6.py
+++ DESAFIO6.py
@@ -1,1 +1,6 @@
 # Faça um programa que recebe o valor dos catetos de um triangulo e imprima na tela o valor da hipotenusa. Lembre-se que: h² = a²+b², sendo h - hipotenusa a e b- catetos do triângulo.
+catetoa=int(input("Valor do primeiro cateto: "))
+catetob=int(input("Valor do segundo cateto: "))
+hipotenusa=int(catetoa*catetob)/2
+
+print("O valor da hipotenusa desse triângulo é ",hipotenusa )
