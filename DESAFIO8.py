bd1c2005b478b455d19c7d1534737feba0c0588e
Jorge Lago <jorgelago92@gmail.com>
2022-09-26 00:56:22 -0300

    Iniciando Desafio do dia 08

---
+++ DESAFIO8.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia o valor da temperatura em graus Celsius e imprima ela convertida em Fahrenheint.
+#Lembrando que:
+#tf = temperatura fahrenheint
+#tc = temperatura Celsius
+#tf=tc*1.8+32.00
