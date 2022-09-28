eb344d8be1e493594ae8a7a6e3d1db279842a2ab
Jorge Lago <jorgelago92@gmail.com>
2022-09-28 08:32:09 -0300

    Programa de calcular soma de 1 até N

--- DESAFIO5.py
+++ DESAFIO5.py
@@ -1,7 +1,7 @@
 #Faça um programa que receba um número inteiro positivo N e calcule o somatório de 1 até N. Imprima na tela o resultado da soma.
 
-num=int(input("digite um numero:  "))
+numero=int(input("digite um numero:  "))
 soma=0
-for i in range(1,num+1):
+for i in range(1,numero+1):
     soma+=i
-print("O resultado da soma de 1 até "+str(num) + " é " + str(soma))
+print("O resultado da soma de 1 até "+str(numero) + " é " + str(soma))
