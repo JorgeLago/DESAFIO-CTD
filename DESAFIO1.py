5a78243bc9680fad5bee3c8d2a364afc7a19cccb
Jorge Lago <jorgelago92@gmail.com>
2022-09-28 08:39:48 -0300

    Calculador de troco em vendas

--- DESAFIO1.py
+++ DESAFIO1.py
@@ -1,10 +1,6 @@
 #Faça um Programa que entre o preço de um produto e o valor em dinheiro pago. imprima para o usuário o quanto será o troco.
 
-
-valor = float(input("digite o valor do produto. R$"))
-
-pago = float(input("digite o valor pago. R$"))
-
+valor = float(input("digite o valor do produto vendido. R$"))
+pago = float(input("digite o valor que foi pago. R$"))
 troco = round(float(pago - valor))
-
 print('o troco será de R$',troco," .")
