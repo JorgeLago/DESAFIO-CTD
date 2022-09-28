73d1cd00589c2ad89935e86916dba2f667e63f7b
Jorge Lago <jorgelago92@gmail.com>
2022-09-28 00:05:20 -0300

    CONVERSOR DE CELSIUS PARA FAHRENHEINT

--- DESAFIO8.py
+++ DESAFIO8.py
@@ -1,5 +1,6 @@
 #Faça um programa que leia o valor da temperatura em graus Celsius e imprima ela convertida em Fahrenheint.
-#Lembrando que:
-#tf = temperatura fahrenheint
-#tc = temperatura Celsius
+
 #tf=tc*1.8+32.00
+tc=float(input("Qual a temperatura em Celsius?  "))
+tf=float((tc*1.8)+32)
+print("A temperatura em Fahrenheint é ", tf )
