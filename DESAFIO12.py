916ff55e5b45e3f5bbe541f78d417bb8de4c26bf
Jorge Lago <jorgelago92@gmail.com>
2022-10-20 23:49:35 -0300

    Desafio 12 iniciado

--- DESAFIO12.py
+++ DESAFIO12.py
@@ -1,1 +1,4 @@
-#Faça um programa que...
+#Faça um programa que imprima o fatorial de um número digital pelo usuário.
+#Você pode fazer a versão com laço de repetição e/ou com resultado. considere que: n! = n*(n-1)*(n-2)*.....*1
+#Exemplo:   5! = 5*4*3*2*1     
+# 5! = 120
