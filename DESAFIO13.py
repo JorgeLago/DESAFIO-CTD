b1ea97bac30aa4afcc2bf2a0c6733db0f152fe4d
Jorge Lago <jorgelago92@gmail.com>
2022-10-22 13:18:57 -0300

    Resolvendo desafio de calcular Anos

--- DESAFIO13.py
+++ DESAFIO13.py
@@ -1,1 +1,6 @@
-#Faça um programa...
+#Faça um programa que leia nome e idade e imprima quando o usuário irá completar 100 anos.
+nome=input('Qual o seu nome ?:' )
+idade=int(input('Qual a sua idade?: '))
+idade_futura=100-idade
+ano_futuro=idade_futura+2022
+print('{} completará 100 anos, daqui a {} anos, em {}.'.format(nome, idade_futura, ano_futuro))
