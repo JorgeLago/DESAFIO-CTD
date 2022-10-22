7b5a2140c4c5dd09ea485d96ef999c6eed1b4f6f
Jorge Lago <jorgelago92@gmail.com>
2022-10-22 13:32:50 -0300

    Verificador de Impar/Par

--- DESAFIO14.py
+++ DESAFIO14.py
@@ -1,2 +1,8 @@
 
 #Faça um programa que leia um número e imprima se ele é par ou ímpar.
+num=int(input('Digite um número: '))
+resto=(num % 2)
+if resto == 0:
+    print('O número é par')
+else:
+    print('O número é ímpar')
