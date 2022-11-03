74f7a168ecf005248a1585b5bde11e5f8188e574
Jorge Lago <jorgelago92@gmail.com>
2022-11-03 20:56:07 -0300

    Atualizando desafio 23.

--- DESAFIO23.py
+++ DESAFIO23.py
@@ -1,6 +1,6 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com a palavra "santo".
 cidade=str(input('Digite o nome da sua cidade: '))
-cidade=cidade.split(' ')
+cidade=(cidade.lower()).split()
 if cidade[0]=='santo':
     print('A cidade começa com a palavra santo.')
 else:
