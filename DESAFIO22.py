ad100d624425cb5ec4370eb5acc98774ffccdef6
Jorge Lago <jorgelago92@gmail.com>
2022-11-02 22:56:35 -0300

    Adicionando lista de novos desafios

---
+++ DESAFIO22.py
@@ -0,0 +1,34 @@
+#Faça um programa que leia o nome completo de uma pessoa e mostre:
+#a) nome com todas as letras maiusculas:
+#b) nome com todas minúsculas:
+#c)quantas letras ao todo ( sem considerar espaços):
+#d)quantas letras tem o primeiro nome:
+
+
+
+
+#Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados:
+#ex.:
+#numero: 1834
+#unid: 4
+#dezena:3
+#centena:8
+#milhar:1
+
+
+#Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com a palavra "santo".
+#próximo
+
+#Crie uma progama que leia o nome de uma pessoa, e diga se tem "silva" no nome.
+#próximo
+
+#Faça um programa que leia uma frase pelo teclado e mostre:
+#a) quantas vezes aparece a letra  "a".
+#b)em que posição ela aparece a primeira vez.
+#c) em que posição ela aparece a ultima vez.
+
+
+#Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente.
+#ex.: Ana Maria de Souza
+#primeiro=Ana
+#último=Souza
