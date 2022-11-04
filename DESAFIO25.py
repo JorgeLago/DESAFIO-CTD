855d2421ac16e9453829f85fe2e1793a791261b3
Jorge Lago <jorgelago92@gmail.com>
2022-11-03 21:51:10 -0300

    Desafio 25 concluído

---
+++ DESAFIO25.py
@@ -0,0 +1,10 @@
+#Faça um programa que leia uma frase pelo teclado e mostre:
+#a) quantas vezes aparece a letra  "a".
+#b) em que posição ela aparece a primeira vez.
+#c) em que posição ela aparece a ultima vez.
+frase=str(input('Digite uma frase: ')).upper().strip()
+print('A letra A aparece {} vezes.'.format(frase.count('A')))
+
+print('A primeira vez na posição {}'.format(frase.find('A')))
+
+print('A última vez na posição {}'.format(frase.rfind('A')))
