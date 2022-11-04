a415e3b1f949e04bfb4f5d9dfc799b987f1f27d0
Jorge Lago <jorgelago92@gmail.com>
2022-11-04 12:06:08 -0300

    Desafio 27 Concluído

---
+++ DESAFIO27.py
@@ -0,0 +1,13 @@
+#ler um número e multiplicar seus elementos.
+import sys
+import math
+
+
+num = (input('digite um numero: '))
+lista=[]
+for i in num:
+    i=int(i)
+    lista.append(i)
+    resultado=math.prod(lista)
+
+print(resultado)
