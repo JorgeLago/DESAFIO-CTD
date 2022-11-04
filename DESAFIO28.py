d1a73c0f7e18b0fa215276cfa3441dc541bbd9df
Jorge Lago <jorgelago92@gmail.com>
2022-11-04 15:59:27 -0300

    Desafio 28 Concluído

--- DESAFIO28.py
+++ DESAFIO28.py
@@ -3,6 +3,6 @@
 # A multa vai custar R$7,00 por cada Km acima do limite.
 velocidade=float(input('Qual a velocidade do carro? > '))
 if velocidade>80:
-    print('Você ultrapassou a velocidade permitida com {} km/h, e o valor da sua multa vai custar R${}.'.format(velocidade, float((velocidade-80)*7.00)))
+    print('Você ultrapassou a velocidade permitida em {} km/h, e o valor da sua multa vai custar R${}.'.format(float(velocidade-80), float((velocidade-80)*7.00)))
 else:
     print('Você está dentro do limite, Parabéns!')
